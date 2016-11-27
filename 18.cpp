8304a19c885c1cb10531d06b95498756a82a2bb0
Grigorii Latyshev <grigorii.latyshev@cern.ch>
2016-11-28 00:11:40 +0100

    3

--- 18.cpp
+++ 18.cpp
@@ -47,6 +47,12 @@ Record askForNewRecord(){
   return r;
 }
 
+// for <set> and structs
+inline bool operator<(const Record& lhs, const Record& rhs)
+{
+  return (lhs.actor).compare(rhs.actor);
+}
+
 int main(){
   set<Record> Records;
   string choice;
@@ -62,10 +68,29 @@ int main(){
     if (choice.compare("1") == 0){
       // insert data
       Record r = askForNewRecord();
+      Records.insert(r);
     } else if (choice.compare("2") == 0){
       // search data
     } else if (choice.compare("3") == 0){
       // print all data;
+      int i = 1;
+      for (set<Record>::iterator r = Records.begin(); r != Records.end(); r++){
+        cout << "================" << endl;
+        cout << "Record #" << i << endl;
+        cout << " * Actor : " << r->actor << endl;
+        cout << " * Search: " << endl;
+        cout << "   rooms : " << r->apart_search.nrooms << endl;
+        cout << "   square: " << r->apart_search.square << endl;
+        cout << "   floor : " << r->apart_search.floor << endl;
+        cout << "   region: " << r->apart_search.region << endl;
+        cout << " * Exist : " << endl;
+        cout << "   rooms : " << r->apart_exist.nrooms << endl;
+        cout << "   square: " << r->apart_exist.square << endl;
+        cout << "   floor : " << r->apart_exist.floor << endl;
+        cout << "   region: " << r->apart_exist.region << endl;
+        cout << "================" << endl;
+        i++;
+      }
     } else choice = "quit";
   } while (choice.compare("quit"  ) != 0);
 
